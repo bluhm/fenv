114b3ab1113d66cb3484f32f5a8d8640682f3bc2
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-07-25 21:55:43 +0200

    Read fpu env, write it to stdout, read it from stdin, and print it.

---
+++ fdump.c
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+
+#include <err.h>
+#include <fenv.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+	struct {
+		fenv_t	env;
+		char	pad[4];
+	} buf; 
+	fenv_t *env = &buf.env;
+	ssize_t len;
+
+	len = read(0, &buf, sizeof(buf));
+	if (len < 0 )
+		err(1, "read");
+	if (len != sizeof(fenv_t))
+		errx(1, "read len %zd is not size %zu",
+		    len, sizeof(fenv_t));
+
+	printf("control\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__control, env->__x87.__control);
+	printf("status\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__status, env->__x87.__status);
+	printf("tag\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__tag, env->__x87.__tag);
+	printf("others0\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__others[0], env->__x87.__others[0]);
+	printf("others1\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__others[1], env->__x87.__others[1]);
+	printf("others2\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__others[2], env->__x87.__others[2]);
+	printf("others3\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__x87.__others[3], env->__x87.__others[3]);
+	printf("mxcsr\tdfl %08x, env %08x\n",
+	    FE_DFL_ENV->__mxcsr, env->__mxcsr);
+
+	return 0;
+}
